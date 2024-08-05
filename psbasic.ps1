537d5b18d96ef04de4019fd58d95299f8bc53a8a
arturkraina <60615982+arturkraina@users.noreply.github.com>
2024-08-05 23:53:29 +0100

    Add files via upload

---
+++ psbasic.ps1
@@ -0,0 +1,4 @@
+﻿net localgroup "Remote Desktop Users" /add "AzureAD\artur.kraina@aaaauto.cz"
+
+$path = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp'
+Set-ItemProperty -Path $path -Name UserAuthentication -Type DWord -Value 0
