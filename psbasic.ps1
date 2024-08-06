072ee3059e521e59e81a91f4bfe8897d64dc7761
arturkraina <60615982+arturkraina@users.noreply.github.com>
2024-08-06 13:10:16 +0100

    Update psbasic.ps1

--- psbasic.ps1
+++ psbasic.ps1
@@ -1,4 +1,9 @@
 ﻿net localgroup "Remote Desktop Users" /add "AzureAD\artur.kraina@aaaauto.cz"
+﻿net localgroup "Remote Desktop Users" /add "AzureAD\tomas.graffe@aaaauto.cz"
+﻿net localgroup "Remote Desktop Users" /add "AzureAD\dusan.prochazka@aaaauto.cz"
+﻿net localgroup "Remote Desktop Users" /add "AzureAD\ondrej.hrudnik@aaaauto.cz"
+net localgroup "Remote Desktop Users" /add "AzureAD\ladislav.fiser@aaaauto.cz"
+net localgroup "Remote Desktop Users" /add "AzureAD\lukas.cernoch@aaaauto.cz"
 
 $path = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp'
 Set-ItemProperty -Path $path -Name UserAuthentication -Type DWord -Value 0
